21a567744aa6453bb460925d7327023419aff9ac
Maxi Skell <maxiskell@gmail.com>
2023-01-12 17:57:57 +0000

    🚸 No more light bulb

--- lua/maxi/plugins/lsp/lspsaga.lua
+++ lua/maxi/plugins/lsp/lspsaga.lua
@@ -4,6 +4,7 @@ if not saga_status then
 end
 
 saga.init_lsp_saga({
+  code_action_icon = "",
   move_in_saga = { prev = "<c-k>", next = "<c-j>" },
   finder_action_keys = { open = "<cr>" },
   definition_action_keys = { edit = "<cr>" },
