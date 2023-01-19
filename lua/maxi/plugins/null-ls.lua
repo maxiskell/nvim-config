f59c5e5988cc2290ec95514a3ac5f66410ceeb71
Maxi Skell <maxiskell@gmail.com>
2023-01-19 15:44:57 +0000

    🔧 Add `gitsigns` to `null_ls` sources

--- lua/maxi/plugins/null-ls.lua
+++ lua/maxi/plugins/null-ls.lua
@@ -11,6 +11,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.rustfmt,
+    null_ls.builtins.code_actions.gitsigns
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
