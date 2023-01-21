71807e8de88e506856d4e4c4daad1276debd0338
Maxi Skell <maxiskell@gmail.com>
2023-01-21 18:00:34 +0000

    🔧 Add stylua config

--- lua/maxi/plugins-setup.lua
+++ lua/maxi/plugins-setup.lua
@@ -1,9 +1,9 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
     return true
   end
   return false
@@ -75,13 +75,13 @@ return packer.startup(function(use)
   use("jose-elias-alvarez/null-ls.nvim")
 
   -- astro
-  use('wuelnerdotexe/vim-astro')
+  use("wuelnerdotexe/vim-astro")
 
   -- cosmic ui
   use("MunifTanjim/nui.nvim")
   use("CosmicNvim/cosmic-ui")
 
   if packer_bootstrap then
-    require('packer').sync()
+    require("packer").sync()
   end
 end)
